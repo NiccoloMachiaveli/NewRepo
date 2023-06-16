11d48f6b06c8a50b902dedb8ae0cd622c9f619c4
Niccolo <Koroliskander@yandex.ru>
2023-06-16 13:37:35 +0400

    Вывод таблицы Product

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -20,9 +20,32 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        BookClubBaseEntities entities = new BookClubBaseEntities();
+        List<Product> OrderProductL = new List<Product>();
         public MainWindow()
         {
             InitializeComponent();
+            dataGridProduct.ItemsSource = entities.Product.ToList();
+            btnOpen.Visibility = Visibility.Hidden;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = dataGridProduct.SelectedItem as Product;
+            if (selectedItem != null)
+            {
+                OrderProductL.Add(selectedItem);
+                btnOpen.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                MessageBox.Show("Выберите товар", "Внимание", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
